757e5b847ccc23ce3eacfd1bfbb6f6532d7abae3
gyeommm <96188162+gyeommm@users.noreply.github.com>
2024-07-15 13:41:06 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -9,8 +9,10 @@ public class Main {
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[i].length; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1] + (s1.charAt(i - 1) == s2.charAt(j - 1) ? 1 : 0),
-                        Math.max(dp[i - 1][j], dp[i][j - 1]));
+                if(s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
