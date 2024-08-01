f1d49177dc9fb22729a68c808fcf8c3032e2f887
gyeommm <96188162+gyeommm@users.noreply.github.com>
2024-08-01 14:30:36 +0900

    [level 2] Title: 모음 사전, Time: 7.77 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,29 @@
 class Solution {
-    static int count = 0, answer = 0;
-    static boolean check = false;
+    static int answer = 0;
     static char[] alpha = {'A', 'E', 'I', 'O', 'U'};
-    static StringBuilder s = new StringBuilder();
     static String w;
 
-    public static void f(StringBuilder now) {
-        if(w.equals(String.valueOf(now))){
-            answer = count;
-            check = true;
-            return;
-        }
+    public static boolean f(String now) {
+        if(w.equals(now))
+            return true;
 
-        if(check || now.length() == 5)
-            return;
+        if(now.length() == 5)
+            return false;
 
         for(int i = 0; i < 5; i++){
             StringBuilder next = new StringBuilder(now);
             next.append(alpha[i]);
-            count++;
-            f(next);
+            answer++;
+            if(f(String.valueOf(next)))
+                return true;
         }
+        
+        return false;
     }
 
     public static int solution(String word) {
         w = word;
-        f(new StringBuilder(""));
+        f("");
         return answer;
     }
 }
