ca44e8800a177818dda36625b8271643fc794bc7
gyeommm <96188162+gyeommm@users.noreply.github.com>
2024-08-06 13:20:39 +0900

    [Gold I] Title: 구슬 탈출, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
@@ -1,93 +1,107 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int[][] d = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    static int n, m;
     static char[][] arr;
-    static int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static int result = 100;
+    static int[][] ball = new int[2][2];
 
-    public static int[] move(int r, int c, int direct, int r2, int c2) {
-        int nr = r;
-        int nc = c;
+    public static void move(int b1, int direct, int b2){
+        while(true){
+            int[] next = {ball[b1][0] + d[direct][0], ball[b1][1] + d[direct][1]};
 
-        while (true) {
-            nr += d[direct][0];
-            nc += d[direct][1];
+            if(arr[next[0]][next[1]] == '#')
+                break;
+            if(next[0] == ball[b2][0] && next[1] == ball[b2][1])
+                break;
+            if(arr[next[0]][next[1]] == 'O'){
+                ball[b1][0] = 0;
+                ball[b1][1] = 0;
+                break;
+            }
 
-            if((nr == r2 && nc == c2) || arr[nr][nc] == '#')
-                return new int[]{nr - d[direct][0], nc - d[direct][1]};
-
-            if(arr[nr][nc] == 'O')
-                return new int[]{0, 0};
+            ball[b1][0] = next[0];
+            ball[b1][1] = next[1];
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bbr = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bbr.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         arr = new char[n][m];
-        int[] red = new int[2];
-        int[] blue = new int[2];
 
-        for (int i = 0; i < n; i++) {
-            String temp = bbr.readLine();
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++){
+            String temp = br.readLine();
+            for(int j = 0; j < m; j++){
                 arr[i][j] = temp.charAt(j);
 
-                if(arr[i][j] == 'R') {
-                    red[0] = i;
-                    red[1] = j;
+                if(arr[i][j] == 'R'){
+                    ball[0][0] = i;
+                    ball[0][1] = j;
                     arr[i][j] = '.';
                 }
-                else if(arr[i][j] == 'B') {
-                    blue[0] = i;
-                    blue[1] = j;
+                else if(arr[i][j] == 'B'){
+                    ball[1][0] = i;
+                    ball[1][1] = j;
                     arr[i][j] = '.';
                 }
             }
         }
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{red[0], red[1], blue[0], blue[1], 0, 0});
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{ball[0][0], ball[0][1], ball[1][0], ball[1][1], -1, 0});
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            ball[0][0] = now[0];
+            ball[0][1] = now[1];
+            ball[1][0] = now[2];
+            ball[1][1] = now[3];
 
-            int rr = cur[0];
-            int rc = cur[1];
-            int br = cur[2];
-            int bc = cur[3];
-            int count = cur[4];
-            int direct = cur[5];
-
-            if (count >= 10)
+            if(now[5] >= 10)
                 continue;
 
-            for (int i = 1; i <= 4; i++) {
-                if(i == direct)
+            for(int i = 0; i < 4; i++){
+                if(now[4] % 2 == i % 2)
                     continue;
 
-                int[] t1 = move(br, bc, i, rr, rc);
-                if(t1[0] == 0 && t1[1] == 0)
-                    continue;
+                boolean check = false;
 
-                int[] t2 = move(rr, rc, i, t1[0], t1[1]);
+                move(0, i, 1);
+                if(ball[0][0] == 0 && ball[0][1] == 0)
+                    check = true;
 
-                int[] t3 = move(t1[0], t1[1], i, t2[0], t2[1]);
-                if(t3[0] == 0 && t3[1] == 0)
-                    continue;
-
-                if (t2[0] == 0 && t2[1] == 0) {
-                    result = Math.min(result, count + 1);
+                move(1, i, 0);
+                if (ball[1][0] == 0 && ball[1][1] == 0) {
+                    ball[0][0] = now[0];
+                    ball[0][1] = now[1];
+                    ball[1][0] = now[2];
+                    ball[1][1] = now[3];
                     continue;
                 }
 
-                queue.add(new int[]{t2[0], t2[1], t3[0], t3[1], count + 1, i});
+                if (!check) {
+                    move(0, i, 1);
+                    if(ball[0][0] == 0 && ball[0][1] == 0)
+                        check = true;
+                }
+
+                if(check){
+                    System.out.println(1);
+                    return;
+                }
+
+                if (ball[0][0] != now[0] || ball[0][1] != now[1] || ball[1][0] != now[2] || ball[1][1] != now[3]) {
+                    q.add(new int[]{ball[0][0], ball[0][1], ball[1][0], ball[1][1], i, now[5] + 1});
+                    ball[0][0] = now[0];
+                    ball[0][1] = now[1];
+                    ball[1][0] = now[2];
+                    ball[1][1] = now[3];
+                }
             }
         }
-
-        System.out.println(result == 100 ? 0 : 1);
+        System.out.println(0);
     }
 }
