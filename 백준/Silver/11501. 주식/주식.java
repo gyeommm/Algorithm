f8100cd5f553febe957ad3b2e0234e717f379841
gyeommm <96188162+gyeommm@users.noreply.github.com>
2024-08-22 13:26:24 +0900

    [Silver II] Title: 주식, Time: 1068 ms, Memory: 317176 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -11,33 +11,18 @@ public class Main {
             int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int[][] temp = new int[n][2];
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                temp[i][0] = Integer.parseInt(st.nextToken());
-                temp[i][1] = i;
-                arr[i] = temp[i][0];
-            }
-
-            Arrays.sort(temp, (a, b) -> {
-                if(a[0] == b[0]){
-                    return b[1] - a[1];
-                }
-                return b[0] - a[0];
-            });
+            for (int i = 0; i < n; i++)
+                arr[i] = Integer.parseInt(st.nextToken());
 
             long result = 0;
-            for (int i = 0, index = 0; i < n && index < n; i++) {
-                if(i < temp[index][1] && arr[i] < temp[index][0]) {
-                    result += temp[index][0] - arr[i];
+            int max = arr[n - 1];
+            for(int i = n - 2; i >= 0; i--) {
+                if(arr[i] < max){
+                    result += max - arr[i];
                     continue;
                 }
-
-                if (arr[i] >= temp[index][0])
-                    continue;
-
-                index++;
-                i--;
+                max = arr[i];
             }
 
             sb.append(result).append("\n");
