897e7c87d1495ab96a3de145b605c50c51108d83
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-01-27 16:35:12 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -9,14 +9,11 @@ int main(){
     
     vector<int> v(10, 0);
     for(int i = 0; i < s.length(); i++){
-        int n = s[i] - '0';
-        if(n == 6 || n == 9){
-            n = 6;
-            if(v[6] > v[9])
-                n = 9;
-        }
-        v[n]++;
+        v[s[i] - '0']++;
     }
+    
+    v[6] = (v[6] + v[9] + 1) / 2;
+    v[9] = v[6];
     cout << *max_element(v.begin(), v.end());
     return 0;
 }
