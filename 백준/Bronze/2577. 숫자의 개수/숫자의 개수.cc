0d42e5ee75f027ac2bafe3c05368f6a5de054fbe
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-01-27 16:10:58 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -2,25 +2,20 @@
 #include <string>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int arr[13];
-	for (int i = 0; i < 10; i++)
-		arr[i] = 0;
-	cin >> arr[10] >> arr[11] >> arr[12];
-
-	string result = to_string(arr[10] * arr[11] * arr[12]);
-
-	int n;
-	for (int i = 0; i < result.length(); i++) {
-		n = result[i] - '0';
-		arr[n]++;
-	}
-
-	for (int i = 0; i < 10; i++)
-		cout << arr[i] << '\n';
-
-	return 0;
+int main(){
+    int a, b, c, mul;
+    cin >> a >> b >> c;
+    mul = a * b * c;
+    
+    string s = to_string(mul);
+    int arr[10] = {0,};
+    for(int i = 0; i < s.length(); i++){
+        arr[s[i] - '0']++;
+    }
+    
+    for(int i = 0; i < 10; i++){
+        cout << arr[i] << '\n';
+    }
+    
+    return 0;
 }
