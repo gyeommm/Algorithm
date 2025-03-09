9e28cf0b5287392800c460acb350d2056df88bff
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-03-09 20:29:11 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 28 ms, Memory: 7064 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -7,11 +7,11 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, m, count = 0, minus = 0;
+    int n, m, count = 0, dupli = 0;
     cin >> n >> m;
     
     vector<string> v(n);
-    vector<vector<int>> visit(n, vector<int>(m, -1));
+    vector<vector<int>> visit(n, vector<int>(m));
     map<char, pair<int, int>> d = {{'U', {-1, 0}}, {'D', {1, 0}}, {'L', {0, -1}}, {'R', {0, 1}}};
     
     for(int i = 0; i < n; i++)
@@ -19,25 +19,25 @@ int main(){
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            if(visit[i][j] != -1)
+            if(visit[i][j] != 0)
                 continue;
             
             count++;
             int r = i;
             int c = j;
-            while(visit[r][c] == -1){
+            while(visit[r][c] == 0){
                 visit[r][c] = count;
                 
                 char nd = v[r][c];
                 r += d[nd].first;
                 c += d[nd].second;
 
-                if(visit[r][c] < count && visit[r][c] > -1)
-                    minus++;
+                if(visit[r][c] < count && visit[r][c] > 0)
+                    dupli++;
             }
         }
     }
     
-    cout << count - minus;
+    cout << count - dupli;
     return 0;
 }
