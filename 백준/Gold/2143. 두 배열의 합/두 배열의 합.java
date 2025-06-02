4a1a8304f5a84163bc033163eca02c0b738cd471
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-06-02 18:22:45 +0900

    [Gold III] Title: 두 배열의 합, Time: 476 ms, Memory: 96644 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -7,30 +7,30 @@ class Main{
         int t = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] a = new int[n + 1];
+        int[] aSum = new int[n + 1];
         for(int i = 1; i <= n; i++){
-            a[i] = a[i - 1] + Integer.parseInt(st.nextToken());
+            aSum[i] = aSum[i - 1] + Integer.parseInt(st.nextToken());
         }
         
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int[] b = new int[m + 1];
+        int[] bSum = new int[m + 1];
         for(int i = 1; i <= m; i++){
-            b[i] = b[i - 1] + Integer.parseInt(st.nextToken());
+            bSum[i] = bSum[i - 1] + Integer.parseInt(st.nextToken());
         }
         
         Map<Integer, Integer> aMap = new HashMap<>();
         for(int i = 1; i <= n; i++){
             for(int j = 0; j < i; j++){
-                int key = a[i] - a[j];
-                aMap.compute(key, (k, v) -> v == null ? 1 : v + 1);
+                int key = aSum[i] - aSum[j];
+                aMap.put(key, aMap.getOrDefault(key, 0) + 1);
             }
         }
         Map<Integer, Integer> bMap = new HashMap<>();
         for(int i = 1; i <= m; i++){
             for(int j = 0; j < i; j++){
-                int key = b[i] - b[j];
-                bMap.compute(key, (k, v) -> v == null ? 1 : v + 1);
+                int key = bSum[i] - bSum[j];
+                bMap.put(key, bMap.getOrDefault(key, 0) + 1);
             }
         }
         
