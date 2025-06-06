d69d3a6acd602000900ed45cff0260721857687d
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-06-06 22:24:38 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 140 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -5,22 +5,23 @@ class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         Deque<int[]> dq = new ArrayDeque<>();
         for(int i = 1; i <= n; i++){
             dq.addLast(new int[]{i, Integer.parseInt(st.nextToken())});
         }
-        
+
         StringBuilder sb = new StringBuilder();
         while(true){
             int[] balloon = dq.pollFirst();
             sb.append(balloon[0]).append(" ");
-            
+
             if(dq.isEmpty()){
                 break;
             }
-            
+
+//            balloon[1] %= dq.size();
             if(balloon[1] < 0){
                 while(balloon[1]++ < 0){
                     dq.addFirst(dq.pollLast());
