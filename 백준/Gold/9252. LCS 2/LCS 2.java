fc2882f43c147f8b848d6f58f09239032afe3e75
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-07-02 18:47:54 +0900

    [Gold IV] Title: LCS 2, Time: 180 ms, Memory: 55216 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static class Point{
+        int l;
+        int r;
+        int c;
+
+        Point(int l, int r, int c){
+            this.l = l;
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        Point[][] dp = new Point[s1.length() + 1][s2.length() + 1];
+        for(int i = 0; i <= s1.length(); i++){
+            dp[i][0] = new Point(0, 0, 0);
+        }
+        for(int j = 0; j <= s2.length(); j++){
+            dp[0][j] = new Point(0, 0, 0);
+        }
+
+        for(int i = 1; i <= s1.length(); i++){
+            for(int j = 1; j <= s2.length(); j++){
+                dp[i][j] = new Point(dp[i - 1][j - 1].l, i - 1, j - 1);
+
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j].l++;
+                    continue;
+                }
+
+                if(dp[i - 1][j].l > dp[i][j].l){
+                    dp[i][j] = new Point(dp[i - 1][j].l, i - 1, j);
+                }
+                if(dp[i][j - 1].l > dp[i][j].l){
+                    dp[i][j] = new Point(dp[i][j - 1].l, i, j - 1);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int r = s1.length();
+        int c = s2.length();
+        if(dp[r][c].l > 0) {
+            while (r != 0 && c != 0) {
+                if (s1.charAt(r - 1) == s2.charAt(c - 1)) {
+                    sb.append(s1.charAt(r - 1));
+                }
+
+                int tempR = dp[r][c].r;
+                int tempC = dp[r][c].c;
+                r = tempR;
+                c = tempC;
+            }
+            sb.append("\n");
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(dp[s1.length()][s2.length()].l).append(sb.reverse());
+        System.out.print(result);
+    }
+}
