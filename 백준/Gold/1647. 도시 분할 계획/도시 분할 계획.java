ebe220b6e839889ef66d410d5c02ac5a03508619
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-07-02 23:00:56 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2656 ms, Memory: 328972 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int[] p;
+
+    static class Edge{
+        int s;
+        int e;
+        int w;
+
+        Edge(int s, int e, int w){
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
+    }
+
+    static int find(int v){
+        if(p[v] == v){
+            return v;
+        }
+        return p[v] = find(p[v]);
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        p = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            p[i] = i;
+        }
+
+        Edge[] e = new Edge[m];
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            e[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(e, (a, b) -> a.w - b.w);
+
+        int result = 0;
+        int max = 0;
+        for(Edge now : e){
+            int sp = find(now.s);
+            int ep = find(now.e);
+            if(sp == ep){
+                continue;
+            }
+
+            if(sp > ep){
+                p[sp] = ep;
+            }else{
+                p[ep] = sp;
+            }
+            result += now.w;
+            max = Math.max(max, now.w);
+        }
+        System.out.print(result - max);
+    }
+}
