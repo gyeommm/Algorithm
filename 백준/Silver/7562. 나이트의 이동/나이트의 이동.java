b246f99e9c4a3083bf0ccb31f3b1440df7a9f04c
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-07-13 19:57:29 +0900

    [Silver I] Title: 나이트의 이동, Time: 484 ms, Memory: 212552 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -23,11 +23,9 @@ class Main{
             q.offer(new int[]{r, c});
             visited[r][c] = 1;
             
-            while(!q.isEmpty()){
+            boolean check = true;
+            while(check && !q.isEmpty()){
                 int[] now = q.poll();
-                if(now[0] == gr && now[1] == gc){
-                    break;
-                }
                 
                 for(int i = 0; i < 8; i++){
                     int[] next = {now[0] + d[i][0], now[1] + d[i][1]};
@@ -36,8 +34,13 @@ class Main{
                         continue;
                     }
                     
-                    q.offer(next);
                     visited[next[0]][next[1]] = visited[now[0]][now[1]] + 1;
+                    if(next[0] == gr && next[1] == gc){
+                        check = false;
+                        break;
+                    }
+                    
+                    q.offer(next);
                 }
             }
             sb.append(visited[gr][gc] - 1).append("\n");
