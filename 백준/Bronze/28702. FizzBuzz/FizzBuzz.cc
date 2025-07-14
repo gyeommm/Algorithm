e6d94a291cbecbb37ac4bba2869d21c036546fec
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-07-14 23:11:03 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int isNumber(string s){
+    try{
+        return stoi(s);
+    }catch(...){
+        return 0;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    for(int i = 0; i < 3; i++){
+        string s;
+        cin >> s;
+        int temp = isNumber(s);
+        
+        if(temp != 0){
+            n = temp + (3 - i);
+        }
+    }
+    
+    string result = to_string(n);
+    if(n % 3 == 0){
+        if(n % 5 == 0){
+            result = "FizzBuzz";
+        }else{
+            result = "Fizz";
+        }
+    }else if(n % 5 == 0){
+        result = "Buzz";
+    }
+    
+    cout << result;
+    return 0;
+}
