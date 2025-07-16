c0d5d8ac6fe1764c03293e2110cf09e729213a4c
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-07-16 21:40:26 +0900

    [Gold IV] Title: 전화번호 목록, Time: 664 ms, Memory: 35484 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+
+            String[] arr = new String[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = br.readLine();
+            }
+            Arrays.sort(arr);
+
+            boolean isYes = true;
+            for(int i = 0; i < n - 1; i++){
+                if(arr[i + 1].startsWith(arr[i])){
+                    sb.append("NO\n");
+                    isYes = false;
+                    break;
+                }
+            }
+            if(isYes){
+                sb.append("YES\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
