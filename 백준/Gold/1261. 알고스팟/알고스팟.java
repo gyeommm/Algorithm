07928d2575cb58fab7377e89015fd25d59887a29
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-08-20 18:28:54 +0900

    [Gold IV] Title: 알고스팟, Time: 152 ms, Memory: 21568 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -22,33 +22,30 @@ class Main{
         for(int i = 0; i < n; i++){
             Arrays.fill(visited[i], 100001);
         }
-        Queue<int[]> q = new ArrayDeque<>();    // {r, c, count}
-        q.offer(new int[]{0, 0, 0});
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0});
         visited[0][0] = 0;
 
         while(!q.isEmpty()){
             int[] now = q.poll();
 
-            if(now[2] > visited[now[0]][now[1]]){
-                continue;
-            }
-
             for(int i = 0; i < 4; i++){
-                int[] next = {now[0] + d[i][0], now[1] + d[i][1], now[2]};
+                int[] next = {now[0] + d[i][0], now[1] + d[i][1]};
 
                 if(next[0] < 0 || next[0] >= n || next[1] < 0 || next[1] >= m){
                     continue;
                 }
 
+                int count = visited[now[0]][now[1]];
                 if(arr[next[0]][next[1]] == 1){
-                    next[2]++;
+                    count++;
                 }
 
-                if(visited[next[0]][next[1]] <= next[2]){
+                if(visited[next[0]][next[1]] <= count){
                     continue;
                 }
 
-                visited[next[0]][next[1]] = next[2];
+                visited[next[0]][next[1]] = count;
                 q.offer(next);
             }
         }
