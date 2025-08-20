ff4e504d795b242dc92d982dfa2be2220450af37
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-08-20 18:58:10 +0900

    [Gold IV] Title: 알고스팟, Time: 144 ms, Memory: 16648 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -22,31 +22,30 @@ class Main{
         for(int i = 0; i < n; i++){
             Arrays.fill(visited[i], 100001);
         }
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0});
-        visited[0][0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {return a[2] - b[2];});    // {r, c, count}
+        pq.offer(new int[]{0, 0, 0});
 
-        while(!q.isEmpty()){
-            int[] now = q.poll();
+        while(!pq.isEmpty()){
+            int[] now = pq.poll();
+
+            if(now[2] >= visited[now[0]][now[1]]){
+                continue;
+            }
+            visited[now[0]][now[1]] = now[2];
 
             for(int i = 0; i < 4; i++){
-                int[] next = {now[0] + d[i][0], now[1] + d[i][1]};
+                int[] next = {now[0] + d[i][0], now[1] + d[i][1], now[2]};
 
                 if(next[0] < 0 || next[0] >= n || next[1] < 0 || next[1] >= m){
                     continue;
                 }
 
-                int count = visited[now[0]][now[1]];
-                if(arr[next[0]][next[1]] == 1){
-                    count++;
-                }
-
-                if(visited[next[0]][next[1]] <= count){
+                next[2] += arr[next[0]][next[1]];
+                if(next[2] >= visited[next[0]][next[1]]){
                     continue;
                 }
 
-                visited[next[0]][next[1]] = count;
-                q.offer(next);
+                pq.offer(next);
             }
         }
 
