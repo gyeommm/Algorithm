71d0e02ba88df101dab61a1248ea8665820a6c92
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-08-25 16:33:11 +0900

    [Gold III] Title: 앱, Time: 128 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -9,33 +9,31 @@ class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        
-        int[][] app = new int[n + 1][2];
-        int cost = 0;
-        for(int i = 1; i <= n; i++){
-            app[i][0] = Integer.parseInt(st1.nextToken());
-            app[i][1] = Integer.parseInt(st2.nextToken());
-            cost += app[i][1];
+        int[][] app = new int[n][2];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            app[i][0] = Integer.parseInt(st.nextToken());
         }
         
-        int[][] dp = new int[n + 1][cost + 1];
-        int result = Integer.MAX_VALUE;
-        for(int i = 1; i <= n; i++){
-            for(int j = 0; j <= cost; j++){
-                if(j < app[i][1]){
-                    dp[i][j] = dp[i - 1][j];
-                    continue;
-                }
-                
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - app[i][1]] + app[i][0]);
-                if(dp[i][j] >= m){
-                    result = Math.min(result, j);
-                }
+        int costSum = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            app[i][1] = Integer.parseInt(st.nextToken());
+            costSum += app[i][1];
+        }
+        
+        int[] dp = new int[costSum + 1];
+        for(int i = 0; i < n; i++){
+            for(int j = costSum; j >= app[i][1]; j--){
+                dp[j] = Math.max(dp[j], dp[j - app[i][1]] + app[i][0]);
             }
         }
         
-        System.out.print(result);
+        for(int i = 0; i <= dp.length; i++){
+            if(dp[i] >= m) {
+                System.out.print(i);
+                break;
+            }
+        }
     }
 }
