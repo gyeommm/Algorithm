13f1130363d3a1cdf62de71d2a3ae7d679ea415c
gyeommm <96188162+gyeommm@users.noreply.github.com>
2025-10-29 14:50:44 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.05 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,19 +1,19 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        
         int[] server = new int[24];
         
         for(int i = 0; i < players.length; i++){
             int add = (players[i] / m) - server[i];
+            if(add <= 0){
+                continue;
+            }
             
-            if(add > 0){
-                answer += add;
-                
-                int end = Math.min(players.length, i + k);
-                for(int j = i; j < end; j++){
-                    server[j] += add;
-                }
+            answer += add;
+            
+            int end = Math.min(players.length, i + k);
+            for(int j = i; j < end; j++){
+                server[j] += add;
             }
         }
         
