23b05801706f9f291aec62dabdcae46bbf4fe8c7
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-01-14 19:16:05 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 255.76 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,70 +1,55 @@
 import java.util.*;
 
 class Solution {
+    static int answer = 0;
+    static int time = 0;
+    static Map<Integer, Boolean>[][] record = new HashMap[101][101];
+    
+    static void move(int[] now, int[] destination, int num){
+        for(; now[num] != destination[num]; time++){
+            if(isCrashed(now)){
+                answer++;
+            }
+
+            if(destination[num] > now[num]){
+                now[num]++;
+            }else{
+                now[num]--;
+            }
+        }
+    }
+    
+    static boolean isCrashed(int[] now){
+        if(record[now[0]][now[1]] == null){
+            record[now[0]][now[1]] = new HashMap<>();
+        }
+
+        Boolean isCrashed = record[now[0]][now[1]].get(time);
+        if(isCrashed == null){
+            record[now[0]][now[1]].put(time, false);
+        }else if(!isCrashed){
+            record[now[0]][now[1]].put(time, true);
+            return true;
+        }
+        
+        return false;
+    }
+
     public int solution(int[][] points, int[][] routes) {
-        int answer = 0;
-        
-        Map<Integer, Boolean>[][] record = new HashMap[101][101];
-        
         for(int i = 0; i < routes.length; i++){
             int start = routes[i][0] - 1;
             int[] now = points[start].clone();
             
-            int time = 0;
+            time = 0;
             for(int j = 1; j < routes[i].length; j++){
                 int end = routes[i][j] - 1;
                 int[] destination = points[end];
 
-                for(; now[0] != destination[0]; time++){
-                    if(record[now[0]][now[1]] == null){
-                        record[now[0]][now[1]] = new HashMap<>();
-                    }
-
-                    Boolean isCrashed = record[now[0]][now[1]].get(time);
-                    if(isCrashed == null){
-                        record[now[0]][now[1]].put(time, false);
-                    }else if(!isCrashed){
-                        record[now[0]][now[1]].put(time, true);
-                        answer++;
-                    }
-
-                    if(destination[0] > now[0]){
-                        now[0]++;
-                    }else{
-                        now[0]--;
-                    }
-                }
-
-                for(; now[1] != destination[1]; time++){
-                    if(record[now[0]][now[1]] == null){
-                        record[now[0]][now[1]] = new HashMap<>();
-                    }
-
-                    Boolean isCrashed = record[now[0]][now[1]].get(time);
-                    if(isCrashed == null){
-                        record[now[0]][now[1]].put(time, false);
-                    }else if(!isCrashed){
-                        record[now[0]][now[1]].put(time, true);
-                        answer++;
-                    }
-
-                    if(destination[1] > now[1]){
-                        now[1]++;
-                    }else{
-                        now[1]--;
-                    }
-                }
+                move(now, destination, 0);
+                move(now, destination, 1);
             }
             
-            if(record[now[0]][now[1]] == null){
-                record[now[0]][now[1]] = new HashMap<>();
-            }
-
-            Boolean isCrashed = record[now[0]][now[1]].get(time);
-            if(isCrashed == null){
-                record[now[0]][now[1]].put(time, false);
-            }else if(!isCrashed){
-                record[now[0]][now[1]].put(time, true);
+            if(isCrashed(now)){
                 answer++;
             }
         }
