8041bbe9b79a42417d523ab39e0f27cf6d8d52cd
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-01-28 22:06:17 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,69 +1,14 @@
 import java.util.*;
 
 class Solution {
-    static class Node{
-        boolean isVisited;
-        List<String> list;
-        
-        Node(){
-            isVisited = false;
-            list = new ArrayList<>();
-        }
-    }
-    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         
-        Map<String, Node> m = new HashMap<>();
-        m.put(begin, new Node());
-        
-        boolean isExist = false;
-        for(String s : words){
-            m.put(s, new Node());
-            if(s.equals(target)){
-                isExist = true;
-            }
-            
-            int count = 0;
-            for(int l = 0; l < begin.length(); l++){
-                if(begin.charAt(l) != s.charAt(l)){
-                    count++;
-                }
-            }
-
-            if(count == 1){
-                m.get(begin).list.add(s);
-                m.get(s).list.add(begin);
-            }
-        }
-        
-        if(!isExist){
-            return 0;
-        }
-        
-        for(int i = 0; i < words.length; i++){
-            String s = words[i];
-            for(int j = i + 1; j < words.length; j++){
-                String ss = words[j];
-                
-                int count = 0;
-                for(int l = 0; l < s.length(); l++){
-                    if(s.charAt(l) != ss.charAt(l)){
-                        count++;
-                    }
-                }
-                
-                if(count == 1){
-                    m.get(s).list.add(ss);
-                    m.get(ss).list.add(s);
-                }
-            }
-        }
-        
         Queue<String> q = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[words.length];
         q.offer(begin);
-        m.get(begin).isVisited = true;
 
+        int level = 0;
         while(!q.isEmpty()){
             int size = q.size();
             
@@ -71,24 +16,35 @@ class Solution {
                 String s = q.poll();
                 if(s.equals(target)){
                     q.clear();
+                    answer = level;
                     break;
                 }
                 
-                Node now = m.get(s);
-                for(String ss : now.list){
-                    Node next = m.get(ss);
-                    if(next.isVisited){
+                for(int j = 0; j < words.length; j++){
+                    if(isVisited[j]){
                         continue;
                     }
-
-                    q.offer(ss);
-                    next.isVisited = true;
+                    
+                    String ss = words[j];
+                    int count = 0;
+                    for(int l = 0; l < ss.length(); l++){
+                        if(s.charAt(l) != ss.charAt(l)){
+                            if(++count > 1){
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if(count == 1){
+                        q.offer(ss);
+                        isVisited[j] = true;
+                    }
                 }
             }
             
-            answer++;
+            level++;
         }
         
-        return answer - 1;
+        return answer;
     }
 }
