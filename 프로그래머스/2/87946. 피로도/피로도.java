01cafd81e1f910ed3ed27b290afe648082d4c427
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-05 21:45:11 +0900

    [level 2] Title: 피로도, Time: 3.28 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,14 +1,14 @@
 class Solution {
     static int answer = -1;
     static boolean[] isVisited;
-    static void f(int k, int[][] dungeons, int count){
+    
+    static void bt(int k, int[][] dungeons, int count){
         for(int i = 0; i < dungeons.length; i++){
-            if(isVisited[i] || k < dungeons[i][0] || k < dungeons[i][1]){
+            if(isVisited[i] || k < dungeons[i][0]){
                 continue;
             }
-            
             isVisited[i] = true;
-            f(k - dungeons[i][1], dungeons, count + 1);
+            bt(k - dungeons[i][1], dungeons, count + 1);
             isVisited[i] = false;
         }
         
@@ -18,7 +18,7 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         isVisited = new boolean[dungeons.length];
         
-        f(k, dungeons, 0);
+        bt(k, dungeons, 0);
         
         return answer;
     }
