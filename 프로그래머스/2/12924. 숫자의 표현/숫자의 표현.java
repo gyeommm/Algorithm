a5529f7b7c0a4e1a8b78a721e16246e50328c1a8
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-07 22:51:33 +0900

    [level 2] Title: 숫자의 표현, Time: 0.18 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        int max = n / 2 + 1;
+        int min = n / 2;
+        
+        int sum = max + min;
+        while(min >= 1){
+            if(sum == n){
+                answer++;
+                sum -= max--;
+                sum += --min;
+            }else if(sum > n){
+                sum -= max--;
+                sum += --min;
+            }else if(sum < n){
+                sum += --min;
+            }
+        }
+        
+        return answer;
+    }
+}
