e05a56dfe714206ad7210fc6a5a40795ce661f92
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-16 17:45:42 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
         int[] count = new int[3];
@@ -7,7 +5,6 @@ class Solution {
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int max = 0;
         for(int i = 0; i < answers.length; i++){
             if(answers[i] == i % 5 + 1){
                 count[0]++;
@@ -20,19 +17,19 @@ class Solution {
             }
         }
         
-        max = Math.max(Math.max(count[0], count[1]), count[2]);
-        int size = 0;
+        int max = Math.max(Math.max(count[0], count[1]), count[2]);
+        int temp = 0;
         for(int i = 0; i < 3; i++){
             if(max == count[i]){
-                size++;
+                temp++;
             }
         }
         
-        int[] answer = new int[size];
-        int index = 0;
+        int[] answer = new int[temp];
+        temp = 0;
         for(int i = 0; i < 3; i++){
             if(max == count[i]){
-                answer[index++] = i + 1;
+                answer[temp++] = i + 1;
             }
         }
         
