e522e0e12474dccb9ba271f6848e4044a2bb649c
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-18 17:20:53 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,33 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
-
-        Set<Integer> s2 = new HashSet<>();
+        
+        List<Integer> list = new ArrayList<>();
+        boolean[] arr = new boolean[31];
+        
         for(int v : reserve){
-            s2.add(v);
+            arr[v] = true;
         }
         
-        Set<Integer> s1 = new TreeSet<>();
         for(int v : lost){
-            if(s2.contains(v)){
-                s2.remove(v);
+            if(arr[v]){
+                arr[v] = false;
                 answer++;
-            }else{
-                s1.add(v);
+                continue;
             }
+            list.add(v);
         }
         
-        for(int v : s1){
-            if(s2.contains(v - 1)){
+        Collections.sort(list);
+        for(int v : list){
+            if(arr[v - 1]){
                 v--;
-            }else if(s2.contains(v + 1)){
+            }else if(arr[v + 1]){
                 v++;
             }else{
                 continue;
             }
             
-            // s1.remove(v);
-            s2.remove(v);
+            arr[v] = false;
             answer++;
         }
         
