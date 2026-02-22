7888b9dfdf6a15e18d02d5dc01bf5e56c6b02ac2
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-23 00:17:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,14 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int answer = n;
-        int k = Integer.bitCount(n);    // 비트 1의 개수 반환
-        
-        while(++answer > 0){
-            if(Integer.bitCount(answer) == k){
-                break;
-            }
-        }
-        
+        int answer = n, k = Integer.bitCount(n);    // 비트 1의 개수 반환
+        while(Integer.bitCount(++answer) != k){}
         return answer;
     }
 }
