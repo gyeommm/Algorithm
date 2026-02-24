d71c09dee9e648419e1a0dd370a6ab7d85c5fb4b
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-24 17:05:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -9,18 +9,14 @@ class Solution {
         for(int i = 1; i < words.length; i++){
             String prev = words[i - 1];
             String now = words[i];
-            answer[1] = i / n + 1;
             
-            if(set.contains(now) || prev.charAt(prev.length() - 1) != now.charAt(0)){
-                answer[0] = i % n + 1;
+            if(!set.add(now) || prev.charAt(prev.length() - 1) != now.charAt(0)){
+                answer = new int[]{i % n + 1, i / n + 1};
                 break;
             }
-            set.add(now);
         }
-
-        if(answer[0] == 0){
-            answer[1] = 0;
-        }
+        
+        answer[1] = answer[0] == 0 ? 0 : answer[1];
         return answer;
     }
 }
