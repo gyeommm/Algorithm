305a4367e6581f158d04dce19f01d396136f12da
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-02-26 22:06:48 +0900

    [level 2] Title: 할인 행사, Time: 33.35 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,41 +1,33 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, Integer> rest = new HashMap<>();
+    
+    static boolean f(){
+        for(Integer n : rest.values()){
+            if(n > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        Map<String, Integer> rest = new HashMap<>();
         for(int i = 0; i < want.length; i++){
             rest.put(want[i], number[i]);
         }
-        
         for(int i = 0; i < 10; i++){
             rest.computeIfPresent(discount[i], (k, v) -> v - 1);
         }
         
-        boolean check = true;
-        for(Integer n : rest.values()){
-            if(n > 0){
-                check = false;
-                break;
-            }
-        }
-        answer += check ? 1 : 0;
-        
         for(int i = 10; i < discount.length; i++){
+            answer += f() ? 1 : 0;
             rest.computeIfPresent(discount[i], (k, v) -> v - 1);
             rest.computeIfPresent(discount[i - 10], (k, v) -> v + 1);
-            
-            check = true;
-            for(Integer n : rest.values()){
-                if(n > 0){
-                    check = false;
-                    break;
-                }
-            }
-            answer += check ? 1 : 0;
         }
         
-        return answer;
+        return answer + (f() ? 1 : 0);
     }
 }
