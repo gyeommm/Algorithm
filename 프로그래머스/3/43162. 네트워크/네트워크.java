ede90f8ffadca2681350a20961fed885cc6612b2
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-04-27 21:01:22 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,26 @@
 class Solution {
-    static boolean[] isVisited = new boolean[200];
-    static void dfs(int n, int[][] computers, int now){
+    static boolean[] isVisited;
+    static void dfs(int[][] computers, int now){
         isVisited[now] = true;
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < isVisited.length; i++){
             if(computers[now][i] == 0 || isVisited[i]){
                 continue;
             }
-            dfs(n, computers, i);
+            dfs(computers, i);
         }
     }
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        isVisited = new boolean[n];
         
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(computers[i][j] == 0 || isVisited[j]){
-                    continue;
-                }
-                dfs(n, computers, j);
-                answer++;
+            if(isVisited[i]){
+                continue;
             }
-        }
-        
-        for(int i = 0; i < n; i++){
-            if(!isVisited[i]){
-                answer++;
-            }
+            dfs(computers, i);
+            answer++;
         }
         
         return answer;
