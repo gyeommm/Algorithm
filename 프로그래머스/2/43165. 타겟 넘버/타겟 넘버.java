683eae03f4f99b661445158447ec08b6d304f2a4
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-04-27 17:43:41 +0900

    [level 2] Title: 타겟 넘버, Time: 4.89 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,19 @@
 class Solution {
     static int answer = 0;
-    
-    void f(int cur, int index, int[] numbers, int target){
-        if(numbers.length == index){
-            if(cur == target)
+    static void bt(int[] numbers, int index, int sum, int target){
+        if(index == numbers.length){
+            if(sum == target){
                 answer++;
+            }
             return;
         }
-                
-        f(cur + numbers[index], index + 1, numbers, target);
-        f(cur - numbers[index], index + 1, numbers, target);
-    }
         
+        bt(numbers, index + 1, sum - numbers[index], target);
+        bt(numbers, index + 1, sum + numbers[index], target);
+    }
+    
     public int solution(int[] numbers, int target) {
-        f(0, 0, numbers, target);
+        bt(numbers, 0, 0, target);
         return answer;
     }
 }
