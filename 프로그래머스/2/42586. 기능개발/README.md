5d02f5823651d258d3aba07536e4a2ec6587dddb
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-04-27 16:16:35 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 81.3 MB, 시간: 0.05 ms
+메모리: 84.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 27일 16:09:01
+2026년 04월 27일 16:16:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> ans = new ArrayList<>();
-        int[] days = new int[progresses.length + 1];
-        days[days.length - 1] = 101;
-        for(int i = 0; i < progresses.length; i++){
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        
+        int[] days = new int[progresses.length];
+        int index = 0;
+        for(int i = 0; i < days.length; i++){
             days[i] = (100 - progresses[i]) / speeds[i];
             days[i] += (100 - progresses[i]) % speeds[i] == 0 ? 0 : 1;
-        }
-        
-        int index = 0;
-        for(int i = 1; i < days.length; i++){
+            
             if(days[index] >= days[i]){
                 continue;
             }
             
-            ans.add(i - index);
+            answer.add(i - index);
             index = i;
         }
         
-        int[] answer = new int[ans.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = ans.get(i);
-        }
+        answer.add(days.length - index);
         return answer;
     }
 }
