4110df2297fb6060d9d5f1a922052ec9ca6a3110
gyeommm <96188162+gyeommm@users.noreply.github.com>
2026-04-28 17:45:13 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,14 +1,7 @@
 -- 코드를 작성해주세요
-select distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
-from developers d
-join skillcodes as s on (d.skill_code & s.code) > 0
-where s.category = 'Front End'
-order by d.id
-
-
-# select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
-# from developers d
-# join (select code
-#         from skillcodes
-#         where category = 'Front End') as c on (d.skill_code & c.code) > 0
-# order by d.id
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from developers
+where skill_code & (select sum(code)
+                    from skillcodes
+                    where category = 'Front End') > 0
+order by id
